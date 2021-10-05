fbd5ee76b4bd012e758044e35e709b74668b6175
LucasMunho <77986683+LucasMunho@users.noreply.github.com>
2021-10-05 10:38:05 -0300

    trazendo apenas numeros impares de um numero recebido na func

--- parOuImpar.playground/Contents.swift
+++ parOuImpar.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Foundation
 
 // Crie uma função que recebe um inteiro como argumento e retorna "Par" para números pares ou "Ímpar" para números ímpares.
 
@@ -13,7 +14,20 @@ func parOuImpar (numero:Int) -> Int{
     
 }
 
-parOuImpar(numero: 1)
-parOuImpar(numero: 2)
-parOuImpar(numero: 3)
-parOuImpar(numero: 4)
+//parOuImpar(numero: 1)
+//parOuImpar(numero: 2)
+//parOuImpar(numero: 3)
+//parOuImpar(numero: 4)
+
+
+func numerosImpares(numero:Int){
+    for numImpar in 0...numero{
+        if numImpar%2 != 0{
+            print(numImpar)
+        }
+    }
+}
+    numerosImpares(numero: 20)
+
+
+
